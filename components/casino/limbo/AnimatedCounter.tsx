1761d578006c7c17c8b4c1ae918614558d1caf41
PrathameshSonje <prathameshsonje147@gmail.com>
2024-07-03 01:30:28 +0530

    past bets reults added

--- components/casino/limbo/AnimatedCounter.tsx
+++ components/casino/limbo/AnimatedCounter.tsx
@@ -1,17 +1,20 @@
 import { animate, KeyframeOptions } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 interface AnimatedCounterProps {
     initial: number,
     final: number,
-    animateOption?: KeyframeOptions;
+    animateOption?: KeyframeOptions,
+    isWinner: boolean
 }
 
-export const AnimatedCounter = ({ initial, final, animateOption }: AnimatedCounterProps) => {
+export const AnimatedCounter = ({ initial, final, animateOption, isWinner }: AnimatedCounterProps) => {
     const ref = useRef<HTMLSpanElement>(null);
+    const divRef = useRef<HTMLDivElement>(null);
 
     useEffect(() => {
         const element = ref.current!;
+        const divElement = divRef.current!;
         if (!element) console.log("no element if referenced");
 
         element.textContent = String(initial);
@@ -25,12 +28,17 @@ export const AnimatedCounter = ({ initial, final, animateOption }: AnimatedCount
             }
         })
 
+        const dynamicClass = isWinner ? "text-green-500" : "text-red-500";
+        const dynamicIcon = isWinner ? "icons8-close-green" : "icons8-close-red";
+        element.className = `font-semibold text-9xl ${dynamicClass}`;
+        divElement.className = `mt-4 ${dynamicIcon}`
+
     }, [final]);
 
     return (
         <div className="flex items-center justify-center">
             <span ref={ref} className="text-white font-semibold text-9xl" />
-            <div className="icons8-close mt-4"></div>
+            <div ref={divRef} className="icons8-close mt-4"></div>
         </div>
     )
 }
